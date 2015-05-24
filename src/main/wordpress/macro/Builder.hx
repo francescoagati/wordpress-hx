05032f7b0fe9a264506131c0c20790827249ee1e
francescoagati <franescoagati1975@gmail.com>
2015-05-24 14:47:29 +0200

    refactoring

--- src/main/wordpress/macro/Builder.hx
+++ src/main/wordpress/macro/Builder.hx
@@ -5,32 +5,31 @@ import haxe.macro.Expr;
 using tink.MacroApi;
 using Lambda;
 
-class Builder {
+@:tink class Builder {
+
+  static function processActions(fields:Array<Field>) {
+    return [for (field in fields){
+      if (field.meta.toMap().exists(':action')) {
+        var meta = field.meta.toMap().get(':action')[0][0];
+        var name = field.name;
+        macro {
+           var fn = function() $i{name}();
+           untyped __call__('add_action',$meta,fn);
+        };
+      }
+    }];
+  }
 
   macro static public function build():Array<Field> {
     var fields = Context.getBuildFields();
     var cls = Context.getLocalClass().toString();
-
-    var blocks = fields.filter(function(field) {
-      return field.meta.toMap().exists(':action');
-    }).map(function(field) {
-      var meta = field.meta.toMap().get(':action')[0][0];
-      var name = field.name;
-      return macro {
-         var fn = function() $i{name}();
-         untyped __call__('add_action',$meta,fn);
-      };
-    });
-
+    var blocks = processActions(fields);
 
     var metaClass = (macro class TTT {
-
        static var h = wordpress.Helper;
-
        static function __init__() {
          $b{blocks};
        }
-
     });
 
     for (field in metaClass.fields) fields.push(field);
