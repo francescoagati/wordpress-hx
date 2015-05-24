5a8c35d28fb3cb2c47a76debca6c5a221ae6a8dd
francescoagati <franescoagati1975@gmail.com>
2015-05-24 15:45:28 +0200

    add shortcode support

--- src/main/wordpress/example/PluginTest.hx
+++ src/main/wordpress/example/PluginTest.hx
@@ -6,9 +6,15 @@ typedef H = wordpress.Helper;
 
 class PluginTest extends wordpress.Plugin {
 
+  @:short_code('hello_world')
+  public static function helloWorld(attrs,content,tag) {
+    attrs.get('test');
+    h.echo("hello wordpress");
+  }
+
   @:action('admin_head')
   public static function addCss() {
-   
+
     h.echo("
       .class {
         background:red;
--- src/main/wordpress/macro/Builder.hx
+++ src/main/wordpress/macro/Builder.hx
@@ -4,13 +4,32 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 using tink.MacroApi;
 using Lambda;
+using wordpress.macro.Builder.BuilderTools;
 
 @:tink class Builder {
 
+  static function processShortCode(fields:Array<Field>) {
+    return [for (field in fields){
+      if (field.meta_exists(':short_code')) {
+        var short_code = field.meta_get(':short_code')[0][0];
+        var name = field.name;
+        macro {
+           var fn = function(attr,content,tag) {
+             //normalize attributes;
+             var new_attr = php.Lib.hashOfAssociativeArray(untyped __php__("shortcode_atts($attr)"));
+             $i{name}(new_attr,content,tag);
+           };
+           untyped __call__('add_shortcode',$short_code,fn);
+        };
+      }
+    }];
+  }
+
+
   static function processActions(fields:Array<Field>) {
     return [for (field in fields){
-      if (field.meta.toMap().exists(':action')) {
-        var meta = field.meta.toMap().get(':action')[0][0];
+      if (field.meta_exists(':action')) {
+        var meta = field.meta_get(':action')[0][0];
         var name = field.name;
         macro {
            var fn = function() $i{name}();
@@ -23,7 +42,7 @@ using Lambda;
   macro static public function build():Array<Field> {
     var fields = Context.getBuildFields();
     var cls = Context.getLocalClass().toString();
-    var blocks = processActions(fields);
+    var blocks = processActions(fields).concat(processShortCode(fields));
 
     var metaClass = (macro class TTT {
        static var h = wordpress.Helper;
@@ -35,6 +54,18 @@ using Lambda;
     for (field in metaClass.fields) fields.push(field);
 
     return fields;
+  }
+
+}
+
+class BuilderTools {
+
+  public static inline  function meta_exists(field:Field,meta:String) {
+    return field.meta.toMap().exists(meta);
+  }
+
+  public static inline  function meta_get(field:Field,meta:String) {
+    return field.meta.toMap().get(meta);
   }
 
 }
